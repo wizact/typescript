7e476d42d0ab84938b823e14a1dc4ae4e6a4a8f4
Amir <amir.mohtasebi@gmail.com>
2015-10-08 22:18:59 +1300

    lambda and this context

---
+++ lambda.js
@@ -0,0 +1,12 @@
+var deck = {
+    suits: ["hearts", "spades", "clubs", "diamonds"],
+    cards: Array(52),
+    createCardPicker: function () {
+        var _this = this;
+        return function () {
+            var pickedCard = Math.floor(Math.random() * 52);
+            var pickedSuit = Math.floor(pickedCard / 13);
+            return { suit: _this.suits[pickedSuit] };
+        };
+    }
+};
---
+++ lambda.ts
@@ -0,0 +1,11 @@
+var deck = {
+    suits: ["hearts", "spades", "clubs", "diamonds"],
+    cards: Array(52),
+    createCardPicker: function() {
+        return () => {
+            var pickedCard = Math.floor(Math.random() * 52);
+            var pickedSuit = Math.floor(pickedCard / 13);
+            return { suit: this.suits[pickedSuit]};
+        }
+    }
+}
