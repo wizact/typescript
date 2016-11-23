9997e628490023c8f4ea26c6ed2e4e13b2eca108
Amir Mohtasebi <amir.mohtasebi@gmail.com>
2016-11-23 14:38:06 +1300

    add static module

--- staticprop.js
+++ staticprop.js
@@ -1,15 +1,19 @@
-var Grid = (function () {
-    function Grid(scale) {
-        this.scale = scale;
-    }
-    Grid.prototype.calculateDistanceFromOrigin = function (point) {
-        var xDist = (point.x - Grid.origin.x);
-        var yDist = (point.y - Grid.origin.y);
-        return Math.sqrt(xDist * xDist + yDist * yDist) / this.scale;
-    };
-    Grid.origin = { x: 0, y: 0 };
-    return Grid;
-}());
-var grid1 = new Grid(1.0);
-grid1.calculateDistanceFromOrigin({ x: 10, y: 10 });
+var StaticProp;
+(function (StaticProp) {
+    var Grid = (function () {
+        function Grid(scale) {
+            this.scale = scale;
+        }
+        Grid.prototype.calculateDistanceFromOrigin = function (point) {
+            var xDist = (point.x - Grid.origin.x);
+            var yDist = (point.y - Grid.origin.y);
+            return Math.sqrt(xDist * xDist + yDist * yDist) / this.scale;
+        };
+        Grid.origin = { x: 0, y: 0 };
+        return Grid;
+    }());
+    var grid = new Grid(1.0);
+    console.log(grid.calculateDistanceFromOrigin({ x: 10, y: 10 }));
+    console.log(grid.calculateDistanceFromOrigin({ x: 6, y: 5 }));
+})(StaticProp || (StaticProp = {}));
 //# sourceMappingURL=staticprop.js.map
--- staticprop.ts
+++ staticprop.ts
@@ -1,13 +1,16 @@
-class Grid {
-    static origin = { x: 0, y: 0 };
-    calculateDistanceFromOrigin(point: { x: number; y: number;}) {
-        var xDist = (point.x - Grid.origin.x);
-        var yDist = (point.y - Grid.origin.y);
+module StaticProp {
+	class Grid {
+	    static origin = { x: 0, y: 0 };
+	    calculateDistanceFromOrigin(point: { x: number; y: number;}) {
+	        var xDist = (point.x - Grid.origin.x);
+	        var yDist = (point.y - Grid.origin.y);
 
-        return Math.sqrt(xDist * xDist + yDist * yDist) / this.scale;
-    }
-    constructor (public scale: number) {}
+	        return Math.sqrt(xDist * xDist + yDist * yDist) / this.scale;
+	    }
+	    constructor (public scale: number) {}
+	}
+
+	var grid = new Grid(1.0);
+	console.log(grid.calculateDistanceFromOrigin({ x: 10, y: 10 }));
+	console.log(grid.calculateDistanceFromOrigin({ x: 6, y: 5 }));
 }
-
-var grid1 = new Grid(1.0);
-grid1.calculateDistanceFromOrigin({ x: 10, y: 10 });
