db65fddd0198a461a47310595cc7bad05de0ca65
Amir Mohtasebi <amir.mohtasebi@gmail.com>
2016-11-23 13:37:52 +1300

    move class example to a module

--- class.js
+++ class.js
@@ -3,53 +3,56 @@ var __extends = (this && this.__extends) || function (d, b) {
     function __() { this.constructor = d; }
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
-var Greeter = (function () {
-    function Greeter(message) {
-        this.greeting = message;
-    }
-    Greeter.prototype.greet = function () {
-        return "Hello, " + this.greeting;
-    };
-    return Greeter;
-}());
-var greeter = new Greeter("world");
-var Animal = (function () {
-    function Animal(theName, extinct) {
-        this.extinct = extinct;
-        this.name = theName;
-    }
-    Animal.prototype.move = function (meters) {
-        if (meters === void 0) { meters = 0; }
-        console.log(this.name + " moved " + meters + "m.");
-    };
-    return Animal;
-}());
-var Snake = (function (_super) {
-    __extends(Snake, _super);
-    function Snake(name) {
-        _super.call(this, name, false);
-    }
-    Snake.prototype.move = function (meters) {
-        if (meters === void 0) { meters = 5; }
-        console.log("Slithering...");
-        _super.prototype.move.call(this, meters);
-    };
-    return Snake;
-}(Animal));
-var Horse = (function (_super) {
-    __extends(Horse, _super);
-    function Horse(name) {
-        _super.call(this, name, false);
-    }
-    Horse.prototype.move = function (meters) {
-        if (meters === void 0) { meters = 45; }
-        console.log("Galloping...");
-        _super.prototype.move.call(this, meters);
-    };
-    return Horse;
-}(Animal));
-var sam = new Snake("Sammy the Python");
-var tom = new Horse("Tommy yhe Palomino");
-sam.move();
-tom.move(34);
+var BasicClass;
+(function (BasicClass) {
+    var Greeter = (function () {
+        function Greeter(message) {
+            this.greeting = message;
+        }
+        Greeter.prototype.greet = function () {
+            return "Hello, " + this.greeting;
+        };
+        return Greeter;
+    }());
+    var greeter = new Greeter("world");
+    var Animal = (function () {
+        function Animal(theName, extinct) {
+            this.extinct = extinct;
+            this.name = theName;
+        }
+        Animal.prototype.move = function (meters) {
+            if (meters === void 0) { meters = 0; }
+            console.log(this.name + " moved " + meters + "m.");
+        };
+        return Animal;
+    }());
+    var Snake = (function (_super) {
+        __extends(Snake, _super);
+        function Snake(name) {
+            _super.call(this, name, false);
+        }
+        Snake.prototype.move = function (meters) {
+            if (meters === void 0) { meters = 5; }
+            console.log("Slithering...");
+            _super.prototype.move.call(this, meters);
+        };
+        return Snake;
+    }(Animal));
+    var Horse = (function (_super) {
+        __extends(Horse, _super);
+        function Horse(name) {
+            _super.call(this, name, false);
+        }
+        Horse.prototype.move = function (meters) {
+            if (meters === void 0) { meters = 45; }
+            console.log("Galloping...");
+            _super.prototype.move.call(this, meters);
+        };
+        return Horse;
+    }(Animal));
+    var sam = new Snake("Sammy the Python");
+    var tom = new Horse("Tommy yhe Palomino");
+    sam.move();
+    tom.move(34);
+})(BasicClass || (BasicClass = {}));
 //# sourceMappingURL=class.js.map
--- class.ts
+++ class.ts
@@ -1,47 +1,49 @@
-class Greeter {
-    greeting: string;
-    constructor(message: string) {
-        this.greeting = message;
+module BasicClass {
+    class Greeter {
+        greeting: string;
+        constructor(message: string) {
+            this.greeting = message;
+        }
+        greet() {
+            return "Hello, " + this.greeting;
+        }
     }
-    greet() {
-        return "Hello, " + this.greeting;
+
+    var greeter = new Greeter("world");
+
+
+    // Inheritance
+
+    class Animal {
+        private name: string;
+        constructor(theName: string, private extinct: boolean) {
+            this.name = theName;
+        }
+
+        move(meters: number = 0) {
+            console.log(this.name + " moved " + meters + "m.");
+        }
     }
+
+    class Snake extends Animal {
+        constructor(name: string) { super(name, false); }
+        move(meters: number = 5) {
+            console.log("Slithering...");
+            super.move(meters);
+        }
+    }
+
+    class Horse extends Animal {
+        constructor(name: string) { super(name, false); }
+        move(meters = 45) {
+            console.log("Galloping...");
+            super.move(meters);
+        }
+    }
+
+    var sam = new Snake("Sammy the Python");
+    var tom: Animal = new Horse("Tommy yhe Palomino");
+
+    sam.move();
+    tom.move(34);
 }
-
-var greeter = new Greeter("world");
-
-
-// Inheritance
-
-class Animal {
-    private name: string;
-    constructor(theName: string, private extinct: boolean) {
-        this.name = theName;
-    }
-
-    move(meters: number = 0) {
-        console.log(this.name + " moved " + meters + "m.");
-    }
-}
-
-class Snake extends Animal {
-    constructor(name: string) { super(name, false); }
-    move(meters: number = 5) {
-        console.log("Slithering...");
-        super.move(meters);
-    }
-}
-
-class Horse extends Animal {
-    constructor(name: string) { super(name, false); }
-    move(meters = 45) {
-        console.log("Galloping...");
-        super.move(meters);
-    }
-}
-
-var sam = new Snake("Sammy the Python");
-var tom: Animal = new Horse("Tommy yhe Palomino");
-
-sam.move();
-tom.move(34);
